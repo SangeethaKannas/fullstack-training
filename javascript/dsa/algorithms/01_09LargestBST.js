4bc15af3b963254cb8925bc8469b902967f8a79a
Sangeetha Kanna <sangeetha.kanna@hcl.com>
2022-10-27 10:46:37 +0530

    Adding DSA in javascript

--- javascript/dsa/algorithms/01_09LargestBST.js
+++
@@ -1,4 +0,0 @@
-/**
- * 
- * You are given the root of a binary tree. Find and return the largest subtree of that tree, which is a valid binary search tree.
- */
--- javascript/dsa/algorithms/23.9smallestpositiveinteger.js
+++
@@ -1,8 +0,0 @@
-/**
-
-You are given an array of integers. Return the smallest positive integer that is not present in the array. The array may contain duplicate entries.
-
-For example, the input [3, 4, -1, 1] should return 2 because it is the smallest positive integer that doesn't exist in the array.
-
-*/
-
